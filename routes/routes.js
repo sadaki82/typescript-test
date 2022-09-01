04a7b3da63b9fce294db394682956ec2b8f387b7
sadakiawada <spark3xxx@gmail.com>
2022-09-01 16:48:52 +0900

    feat: add mongodb

---
+++ routes/routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const foodController = require("../controllers/food");
+
+app.get("/api/foods", foodController.getFoods);
+
+app.get("/api/foods/:idOrName", foodController.getFood);
+
+app.patch("/api/foods/:idOrName", foodController.patchFood);
+
+app.delete("/api/foods/:idOrName", foodController.deleteFood);
+
+app.post("/api/foods", foodController.postFood);
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json()); // application/json
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const foodRouter = require("../routes/routes");
+
+app.use(foodRouter);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("データ接続成功"))
+  .catch((err) => console.log(err));
 
 const setupServer = () => {
-  app.use("/", (req, res) => {
-    res.send("hello");
-  });
+  // app.use("/", (req, res) => {
+  //   res.send("hello");
+  // });
+  app.use(foodRouter);
 
   return app;
 };
