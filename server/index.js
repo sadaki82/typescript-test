88529bd845fab785f050f40b41223bdbc167a37c
sadakiawada <spark3xxx@gmail.com>
2022-09-01 17:00:10 +0900

    add: cors setting

--- server/index.js
+++ server/index.js
@@ -2,6 +2,24 @@ const PORT = process.env.PORT || 8000;
 const { setupServer } = require("./server");
 const app = setupServer();
 
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 (async () => {
   try {
     console.log("Running migrations...");
