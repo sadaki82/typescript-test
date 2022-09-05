51abe135ff4c9f1bcbbc265fde033765d8616f9d
sadakiawada <spark3xxx@gmail.com>
2022-09-05 16:37:49 +0900

    fix: model

--- src/models/food.ts
+++ src/models/food.ts
@@ -6,6 +6,17 @@ export interface IFood extends Document {
 
 const FoodSchema: Schema = new mongoose.Schema({
   name: String,
+  category: String,
+  distributor: String,
+  url: String,
+  price: String,
+  evaluation: String,
+  material: String,
+  nutrition: String,
+  description: String,
+  img: String,
+  imgfilename: String,
+  cards: { user_cards: Array, user_favarite: Array },
 });
 
 // const Food = mongoose.model("Food", FoodSchema);
